1f46d5f6b2dc74db93bb9e25d0356d84d0a114bc
Shuja Shah <shuja.ashraf11@gmail.com>
2025-10-25 12:21:17 +0530

    error tracking

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -5,27 +5,47 @@ import { generateText } from 'ai';
 import { createOpenAI } from '@ai-sdk/openai';
 import { createAnthropic } from '@ai-sdk/anthropic';
 
+import * as Sentry from '@sentry/nextjs';
+
 const google = createGoogleGenerativeAI();
 const openai = createOpenAI();
 const anthropic = createAnthropic();
 
 export const execute = inngest.createFunction({ id: 'execute-ai' }, { event: 'execute/ai' }, async ({ event, step }) => {
+  Sentry.logger.info('user triggered test log', {log_source: 'sentry_test'});
+  console.warn('Something is missing');
+  console.error('This is an error i want to track...')
   const { steps: geminiSteps } = await step.ai.wrap('gemini-generate-text', generateText, {
     model: google('gemini-2.5-flash'),
     system: 'You are a helpful assistant.',
     prompt: 'Provide me some next.js interview questions?',
+    experimental_telemetry: {
+      isEnabled: true,
+      recordInputs: true,
+      recordOutputs: true,
+    }
   });
 
   const { steps: openAiSteps } = await step.ai.wrap('openai-generate-text', generateText, {
     model: openai('gpt-4'),
     system: 'You are a helpful assistant.',
     prompt: 'Provide me some next.js interview questions?',
+    experimental_telemetry: {
+      isEnabled: true,
+      recordInputs: true,
+      recordOutputs: true,
+    }
   });
 
   const { steps: anthropicSteps } = await step.ai.wrap('anthropic-generate-text', generateText, {
     model: anthropic('claude-sonnet-4-5'),
     system: 'You are a helpful assistant.',
     prompt: 'Provide me some next.js interview questions?',
+    experimental_telemetry: {
+      isEnabled: true,
+      recordInputs: true,
+      recordOutputs: true,
+    }
   });
 
   return {
